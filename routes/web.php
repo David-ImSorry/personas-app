2f0e370808aa65da717d1a40b7430feaa694b8b3
David-ImSorry <jd.barbetti@gmail.com>
2023-10-18 18:45:21 -0500

    Commit basado

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/comunas', [ComunaController::class, 'index']);
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
