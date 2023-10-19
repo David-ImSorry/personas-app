273e14c324e0e4080dc70eca624877ea7dd680b6
David-ImSorry <jd.barbetti@gmail.com>
2023-10-19 17:07:31 -0500

    Añadir Comuna (Completo)

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -31,7 +31,10 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+        return view('comunas.new', ['municipios' => $municipios]);
     }
 
     /**
@@ -42,7 +45,17 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->Save();
+
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+            ->get();
+        return view('comunas.index', ['comunas' => $comunas]);
     }
 
     /**
