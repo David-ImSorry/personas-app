a48a22ed01531817a2af0bd4be15103528d851ad
David-ImSorry <jd.barbetti@gmail.com>
2023-10-20 17:10:53 -0500

    Edit (POR FIN AAAA)

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -77,7 +77,11 @@ class ComunaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+        return view('comunas.edit', ['comuna' => $comuna, 'municipios'=>$municipios]);
     }
 
     /**
@@ -89,7 +93,17 @@ class ComunaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comuna = new Comuna();
+        
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->Save();
+
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+            ->get();
+        return view('comunas.index', ['comunas' => $comunas]);
     }
 
     /**
--- resources/views/comunas/edit.blade.php
+++ resources/views/comunas/edit.blade.php
@@ -13,29 +13,34 @@
   <body>
     <div class="container">
         <h1>Edit Comune</h1>
-        <form method="post" action="{{ route('comunas.store') }}">
-        @csrf    
+        <form method="POST" action="{{ route('comunas.update', ['comuna' => $comuna->comu_codi]) }}">
+          @method('put')          
+          @csrf    
         <div class="mb-3">
-                <label for="codigo" class="form-label">Code</label>
-                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id"
-                  disabled="disabled">
-                <div id="idHelp" class="form-text">Comune Code</div>
+                <label for="codigo" class="form-label">Id</label>
+                <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id"
+                  disabled="disabled" value="{{ $comuna->comu_codi }}">
+                <div id="codigoHelp" class="form-text">Commune id</div>
             </div>
             <div class="mb-3">
-                <label for="name" class="form-label">Comune</label>
-                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" 
-                  name="name" placeholder="Comuna name.">
+                <label for="name" class="form-label">Commune</label>
+                <input type="text" required class="form-control" id="name" placeholder="Comuna name." 
+                  name="name" value="{{ $comuna->comu_nomb }}">
             </div>
 
             <label for="municipality">Municipality:</label>
             <select class="form-select" id="municipality" name="code" Required>
               <option selected disabled value="">Choose one...</option>
               @foreach ($municipios as $municipio)
-                <option value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
+                @if ($municipio->muni_codi == $comuna->muni_codi)
+                  <option selected value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
+                @else
+                  <option value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
+                @endif
               @endforeach
             </select>
             <div class="mt-3">
-                <button type="submit" class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary">Update</button>
                 <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
             </div>
         </form>
