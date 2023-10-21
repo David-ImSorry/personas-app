10f2bdd9604447439192114c1664ebe3ed646b1a
Dianlly Colorado <msdsc4@gmailcom>
2023-10-20 22:30:24 -0500

    Arreglo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
         <div class="row">
             <div class="col-lg-3 col-md-6 mb-4">
                 <div class="card">
-                    <a href="comuna.html">
+                    <a href="Comuna.php">
                         <img src="comuna-image.jpg" class="card-img-top" alt="Comuna">
                         <div class="card-body">
                             <h5 class="card-title">Comuna</h5>
