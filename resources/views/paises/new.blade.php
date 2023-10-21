66c56072e15d73cc36bbf84abdfd7559253446d3
Dianlly Colorado <msdsc4@gmailcom>
2023-10-20 22:30:41 -0500

    otro arreglo

--- resources/views/paises/new.blade.php
+++ resources/views/paises/new.blade.php
@@ -1,56 +1,84 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Inicio</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Agrega el enlace a Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Add Comuna</title>
-  </head>
-  <body>
+    <style>
+        body {
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            margin-top: 50px;
+        }
+
+        .card {
+            border: none;
+            cursor: pointer;
+        }
+
+        .card:hover {
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-title {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
     <div class="container">
-        <h1>Add Comuna</h1>
-        <form method="post" action="{{ route('comunas.store') }}">
-        @csrf    
-        <div class="mb-3">
-                <label for="id" class="form-label">Code</label>
-                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id"
-                  disabled="disabled">
-                <div id="idHelp" class="form-text">Comune Code</div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card">
+                    <a href="comunas">
+                        <img src="comuna-image.jpg" class="card-img-top" alt="Comuna">
+                        <div class="card-body">
+                            <h5 class="card-title">Comuna</h5>
+                        </div>
+                    </a>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="name" class="form-label">Comune</label>
-                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" 
-                  name="name" placeholder="Comuna name.">
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card">
+                    <a href="departamentos">
+                        <img src="departamentos-image.jpg" class="card-img-top" alt="Departamentos">
+                        <div class="card-body">
+                            <h5 class="card-title">Departamentos</h5>
+                        </div>
+                    </a>
+                </div>
             </div>
-
-            <label for="municipality">Municipality:</label>
-            <select class="form-select" id="municipality" name="code" Required>
-              <option selected disabled value="">Choose one...</option>
-              @foreach ($municipios as $municipio)
-                <option value="{{ $municipio->muni_codi }}">{{ $municipio->muni_nomb }}</option>
-              @endforeach
-            </select>
-            <div class="mt-3">
-                <button type="submit" class="btn btn-primary">Save</button>
-                <a href="{{ route('comunas.index') }}" class="btn btn-warning">Cancel</a>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card">
+                    <a href="paises">
+                        <img src="paises-image.jpg" class="card-img-top" alt="Países">
+                        <div class="card-body">
+                            <h5 class="card-title">Países</h5>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </form>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card">
+                    <a href="municipios">
+                        <img src="municipios-image.jpg" class="card-img-top" alt="Municipios">
+                        <div class="card-body">
+                            <h5 class="card-title">Municipios</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-    
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
+    <!-- Agrega el enlace a Bootstrap JS y jQuery (opcional) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
 </html>
